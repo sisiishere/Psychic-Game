bab4f6c16a744b271693ff4055b9aad53466c0d5
sisiishere <set38246@marymount.edu>
2019-06-14 15:49:13 -0400

    modify

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,5 +1,4 @@
-// Creates an array that lists out all of the options (Rock, Paper, or Scissors).
-var computerChoices = ["l", "g", "b", "t", "q", "i", "a", "p", "n"];
+var alphabet = ["l", "g", "b", "t", "q", "i", "a", "p", "n"];
 
 var array = []
 
@@ -19,110 +18,46 @@ var reset = function(){
     document.getElementById("guessleft").textContent = guessleft;
 }
 
-var x = 1;  {
-    x = 2;
-    x = 3;
-    x = 4;
-    x = 5;
-    x = 6;
-    x = 7;
-    x = 8;
-    x = 9;
-    x = 10;
-    x = 11;
-    x = 12;
-}
-
-// This function is run whenever the user presses a key.
-document.onkeyup = function(event) {
-
-    // Determines which key was pressed.
+var reset = function() {
+    guessleft = 9;
+    guesssofar = [];
+    computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+  };
+  
+  var displayResults = function() {
+    document.getElementById('win').innerHTML = 'Wins: ' + win;
+    document.getElementById('loss').innerHTML = 'Losses: ' + loss;
+    document.getElementById('guessleft').innerHTML =
+      'Guesses Left: ' + guessleft;
+    document.getElementById('guesssofar').innerHTML =
+      'Your guesses so far: ' + array;
+  };
+  
+  var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+  
+  document.onkeyup = function(event) {
     var userGuess = event.key;
-
-    // Randomly chooses a choice from the options array. This is the Computer's guess.
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-
-
-
-
-    // Alerts the key the user pressed (userGuess).
-    console.log("User guess: " + userGuess);
-
-    // Alerts the Computer's guess.
-    console.log("Computer guess: " + computerGuess);
-
-    // document.getElementById("user-choice").innerHTML = event.key;
-
-    // document.getElementById("computer-choice").innerHTML = computerGuess;
-
-    // if the user and comp make the same choice, the user wins a point in win; if the letter is guessed, the letter will show.
-    if (userGuess === computerGuess) {
-    win++;
-    document.getElementById("win").textContent = "Wins: " + win;
-    array.push(event.key);
-    document.getElementById("guesssofar").textContent =  "Your Guesses So Far: " + array; 
-    document.getElementById("guesssofar").textContent = "Guesses Left: 0" + guessleft - 9;
-    var clearButton = document.createElement("BUTTON");
-    var text = document.createTextNode("Clear Selection");
-    clearButton.onclick = function () {
-        guessleft = 9;
-    console.log("Guesses Left: 9")
+    array.push(userGuess);
    
+  
+    console.log(computerGuess);
+  
+    if (userGuess === computerGuess) {
+      win++;
+      reset();
+      console.log('Way to go!');
+    } else {
+      guessleft--;
     }
+  
+    if (guessleft <= 0) {
+      loss++;
+      reset();
+      console.log('You Lost!');
     }
-
-    // if user makes a different choice from the comp, user loses a point in guesses left; if the letter is guessed, the letter will show.
-    else if (userGuess !== computerGuess) {
-    guessleft--;
-    document.getElementById("guessleft").textContent = "Guesses Left: " + guessleft;
-    array.push(event.key);
-    document.getElementById("guesssofar").textContent =  "Your Guesses So Far: " + array; 
-    
-
-    }
-
-    // if user makes a different choice from the comp on the ninth try, user gains a point in loss; if the letter is guessed, the letter will show.
-      if (guessleft === 0) {
-    loss++;
-    document.getElementById("loss").textContent = "Losses: " + loss;
-    array.push(event.key);
-    guessleft = 0;
-    document.getElementById("guesssofar").textContent =  "Your Guesses So Far: " + array; 
-    document.getElementById("guesssofar").textContent = "Guesses Left: 0" + guessleft - 9;
-    var clearButton = document.createElement("BUTTON");
-    var text = document.createTextNode("Clear Selection");
-    clearButton.onclick = function () {
-        guessleft = 9;
-    console.log("Guesses Left: 9");
-
-    }
-    }
-
-    //if user wins, then the guesses left will reset to nine.
-        if (userGuess === computerGuess && win++) {
-         guessleft = 9;
-        document.getElementById("guessleft").textContent = "Guesses Left: 9 " 
-        array = [];
-        document.getElementById("guesssofar").textContent = "Your Guesses So Far:  "
-        array.push(event.key);
-        
-    
-    }
-
-    //if user loses, then the guesses left will reset to nine.
-        else if (guessleft === 0 && loss++) {
-         guessleft = 9;
-        document.getElementById("guessleft").textContent = "Guesses Left: 9 "
-        array = [];
-        document.getElementById("guesssofar").textContent = "Your Guesses So Far:  "
-        array.push(event.key);  
-      
-    
-    }
+    displayResults();
 
 
-    
+    };
 
-
-};
+   
